81ec2da3b1e82d428e54e8eac96077b59856fd89
LorentimoS <90320977+LorentimoS@users.noreply.github.com>
2022-11-30 20:13:54 +0000

    initial commit build react

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+function App(props){
+  return (
+    <main className="main">
+      Hello, {props.txt}!
+    </main>
+  )
+}
+
+export {App}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {React} from 'react'
+import ReactDOM from 'react-dom/client'
+import {App} from "./components/App"
+
+document.addEventListener("DOMContentLoaded", setup)
+
+function setup(){
+  const root = ReactDOM.createRoot(document.getElementById('app'));
+  const element = <App txt = 'friend'/>;
+  root.render(element);
+}
