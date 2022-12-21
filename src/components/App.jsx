e2f714c91bcb2ea5ec6b09b766e8daecc90ef7e6
LorentimoS <90320977+LorentimoS@users.noreply.github.com>
2022-12-21 08:39:59 +0000

    divide set's on diff useEffect()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { Form } from "./Form"
 import { Chart } from "./Chart"
 import { Map } from "./Map"
@@ -7,61 +7,67 @@ import { weatherByCoord } from '../api/weatherByCoord'
 
 function App(props) {
   const [cityName, setCityName] = useState('')
-  const [checkTemp, setCheckTemp] = useState(0)
-  const [checkRain, setCheckRain] = useState(0)
+  const [checkTemp, setCheckTemp] = useState(false)
+  const [checkRain, setCheckRain] = useState(false)
 
-  const [cityInfo, setCityInfo] = useState([]);
-  const [lon, setLon] = useState(0)
-  const [lat, setLat] = useState(0)
+  const [cityInfo, setCityInfo] = useState(undefined);
+  const [lon, setLon] = useState(0.0)
+  const [lat, setLat] = useState(0.0)
 
   const [weatherData, setWeatherData] = useState([])
   const [weatherUnits, setWeatherUnits] = useState([])
 
   const onFormSubmitted = (name, temp, rain) => {
     setCityName(name)
-    setCheckTemp(+temp)
-    setCheckRain(+rain)
+    setCheckTemp(temp)
+    setCheckRain(rain)
   }
 
   useEffect(() => {
     if (cityName != '') {
-      const city = new cityCoord(cityName);
+      const city = new cityCoord(cityName)
       city.info().then(newCityInfo => {
-        setCityInfo(newCityInfo)
+        setCityInfo(newCityInfo[0])
       })
+    }
+  }, [cityName])
 
-      if (cityInfo.length == 0) {
+  useEffect(() => {
+    if (cityName != '') {
+      if (cityInfo == undefined) {
         //alert("Input the real name of city")
         console.log("Unreal name of city")
       }
       else {
-        setLon(cityInfo[0].lon)
-        setLat(cityInfo[0].lat)
-        const cityWeather = new weatherByCoord(lon, lat)
-        cityWeather.weather().then(weatherArray => {
-          const time = weatherArray.hourly.time;
-          const temp = weatherArray.hourly.temperature_2m;
-          const rain = weatherArray.hourly.rain;
-          const arrWeatherData = [];
-          for (let i = 0; i < time.length; i++) {
-            arrWeatherData.push([time[i], temp[i], rain[i]])
-          }
-          setWeatherData(arrWeatherData)
-          setWeatherUnits([weatherArray.hourly_units.temperature_2m,  
-                           weatherArray.hourly_units.rain])
-        })
-        
-        console.log(cityName)
+        setLon(cityInfo.lon)
+        setLat(cityInfo.lat)
       }
     }
-  }, [cityName, checkTemp, checkRain])
+  }, [cityInfo])
+
+  useEffect(() => {
+    if (cityName != '' && cityInfo != undefined) {
+      const cityWeather = new weatherByCoord(lon, lat)
+      cityWeather.weather().then(weatherArray => {
+        const time = weatherArray.hourly.time;
+        const temp = weatherArray.hourly.temperature_2m;
+        const rain = weatherArray.hourly.rain;
+        const arrWeatherData = [];
+        for (let i = 0; i < time.length; i++) {
+          arrWeatherData.push([time[i], temp[i], rain[i]])
+        }
+        setWeatherData(arrWeatherData)
+        setWeatherUnits([weatherArray.hourly_units.temperature_2m,
+        weatherArray.hourly_units.rain])
+      })
+    }
+  }, [lon])
 
   return (
     <main className="main">
       <Form onFormSubmit={onFormSubmitted} />
-      <Chart arrayWeatherData={weatherData} units={weatherUnits} tempr={checkTemp} rain={checkRain}/>
+      <Chart arrayWeatherData={weatherData} units={weatherUnits} tempr={checkTemp} rain={checkRain} />
       <Map longitude={lon} latitude={lat} />
-      <div>{cityName}+{checkTemp}+{checkRain}+{lon}+{lat}</div>
     </main>
   )
 }
